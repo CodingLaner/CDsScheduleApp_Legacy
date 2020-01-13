b7aace32493f8e2cc1d31d70448dae77fe899a6b
CodingLaner <codingLaner@users.noreply.github.com>
2020-01-13 17:33:17 +0900

    구글 애드몹 삽입 애드몹 실행 코드 추가 애드몹 리스너 추가 애드몹 xml 추가 애드몹 ID 및 앱 ID 추가 앱의 인터넷 사용권한 추가 play services ads에 전달할 meta-data 추가 play services ads 18.3.0 패키지 추가

--- app/src/main/java/com/example/cdsscheduleapp/MainActivity.java
+++ app/src/main/java/com/example/cdsscheduleapp/MainActivity.java
@@ -3,12 +3,62 @@ package com.example.cdsscheduleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, getString(R.string.admob_app_id));
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        // 광고 리스너
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // 광고 정상 실행시 출력
+                Log.d("@@@", "onAdLoaded");
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // 광고 비정상 실행시 출력
+                Log.d("@@@", "onAdFailedToLoad " + errorCode);
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+
+            @Override
+            public void onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+        });
     }
 }
